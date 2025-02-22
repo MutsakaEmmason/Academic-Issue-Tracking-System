4677e9a470ac9c3a3415282aca94f7c34c33d839
fredrick01 <kigongobazirafred@gmail.com>
2025-02-22 20:30:38 +0300

    changed app.jsx

--- AITS/frontend/src/App.jsx
+++ AITS/frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import React from "react";
 import LecturerLogin from "./Login";
+import StudentLogin from "./student_login"; // Ensure this import is correct
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-    return (
-        <div>
-            <LecturerLogin />
-        </div>
-    );
+  return (
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/student_login" element={<StudentLogin />} />
+          <Route path="/" element={<LecturerLogin />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
 export default App;
