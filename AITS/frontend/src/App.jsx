2e54afe489c3f38318471b1591187b4d5eb63307
wambi elvis <wambielvis12@gmail.com>
2025-02-24 20:22:16 +0300

    lecturer login

--- AITS/frontend/src/App.jsx
+++ AITS/frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import LecturerLogin from "./Login";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LecturerLogin from "./LecturerLogin.jsx";
+import LecturerDashboard from "./LecturerDashboard.jsx";
+import "./LecturerDashboard.css";
+import "./LecturerLogin.css";
 
 function App() {
     return (
-        <div>
-            <LecturerLogin />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<LecturerLogin />} />
+                <Route path="/lecturer-dashboard" element={<LecturerDashboard />} />
+            </Routes>
+        </Router>
     );
 }
 
