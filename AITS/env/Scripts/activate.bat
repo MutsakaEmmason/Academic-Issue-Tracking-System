0df10d0ebc993fee7851e9a69ad2800888d2b656
Mutsaka Emmason <mutsakaemmason@gmail.com>
2025-02-28 19:16:28 +0300

    added email in signals

--- AITS/env/Scripts/activate.bat
+++ AITS/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\EMMA\Desktop\EMMASON\Academic-Issue-Tracking-SYstem\AITS\env
+set VIRTUAL_ENV=C:\Users\EMMA\Desktop\EMMASON\Academic-Issue-Tracking-System\AITS\env
 
 if not defined PROMPT set PROMPT=$P$G
 
