49b21f16b5fe17fe5684d9e3723e311182de9c1e
wambi elvis <wambielvis12@gmail.com>
2025-03-02 14:46:30 +0300

    modified

--- AITS/frontend/src/App.jsx
+++ AITS/frontend/src/App.jsx
@@ -1,18 +1,34 @@
 
+
+
+jsx
 import React from "react";
-import LecturerLogin from "./Login";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import LecturerLogin from "./LecturerLogin.jsx";
+import LecturerDashboard from "./LecturerDashboard.jsx";
 import StudentLogin from "./student_login";
 import Home from "./home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
 import Register from "./routes/register";
 
 function App() {
-    return (
-        <div>
-            <LecturerLogin />
-        </div>
-    );
+  return (
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lecturer-login" element={<LecturerLogin />} />
+          <Route path="/lecturer-dashboard" element={<LecturerDashboard />} />
+          <Route path="/student-login" element={<StudentLogin />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  );
 }
 
 export default App;
