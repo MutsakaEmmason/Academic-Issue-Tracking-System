9bfb87d46fa1d0c19316ed9c354f7eee858c049e
wambi elvis <wambielvis12@gmail.com>
2025-03-08 06:12:58 +0300

    modified file

--- AITS/frontend/src/App.jsx
+++ AITS/frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // src/App.jsx
 import React from "react";
 import LecturerLogin from "./Login";
@@ -8,21 +7,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import Register from "./routes/register";
 
-=======
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-import LecturerLogin from "./LecturerLogin.jsx";
-import LecturerDashboard from "./LecturerDashboard.jsx";
-import StudentLogin from "./StudentLogin.jsx"; // Ensure correct import
-import Home from "./Home.jsx"; // Ensure correct import
-import Register from "./Register.jsx"; // Ensure correct import
-import StudentDashboard from "./StudentDashboard.jsx";
->>>>>>> origin/fred
 
 function App() {
   return (
@@ -30,18 +14,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
           <Route path="/student/login" element={<StudentLogin />} />
           <Route path="/lecturer/login" element={<LecturerLogin />} />
           {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
           <Route path="/register" element={<Register />} />
-=======
-          <Route path="/lecturer-login" element={<LecturerLogin />} />
-          <Route path="/lecturer-dashboard" element={<LecturerDashboard />} />
-          <Route path="/student-login" element={<StudentLogin />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/student-dashboard" element={<StudentDashboard />} />
->>>>>>> origin/fred
         </Routes>
       </Router>
     </ChakraProvider>
