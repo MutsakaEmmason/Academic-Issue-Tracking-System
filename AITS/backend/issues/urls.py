aea9d655bd81d3c58b0a5402b14c322a5b538d05
Mutsaka Emmason <mutsakaemmason@gmail.com>
2025-03-09 18:46:34 +0300

    added path assign in urls

--- AITS/backend/issues/urls.py
+++ AITS/backend/issues/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include.
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, IssueViewSet, CommentViewSet, NotificationViewSet, AuditLogViewSet
 from .views import IssueAttachmentViewSet
@@ -13,7 +13,7 @@ router.register(r'attachments', IssueAttachmentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-     path('issues/<int:pk>/assign/', IssueViewSet.as_view({'patch': 'assign'})),
+     path('issues/<int:pk>/assign/', IssueViewSet.as_view({'patch': 'assign'})),.
     path('issues/<int:pk>/resolve/', IssueViewSet.as_view({'patch': 'resolve'})),
 ]
 
