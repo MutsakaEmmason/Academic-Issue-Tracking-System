aa0c4fd5a48ed35f364a9f604662ae677212be7a
fredrick01 <kigongobazirafred@gmail.com>
2025-03-11 14:08:09 +0300

    added issuesubmissionform.jsx

---
+++ AITS/frontend/src/components/IssueSubmisionForm.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import {
+    VStack,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Text,
+    Box,
+    Select,
+} from "@chakra-ui/react";
