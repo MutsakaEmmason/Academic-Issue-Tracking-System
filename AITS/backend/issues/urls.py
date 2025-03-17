8eb19a76189ac1d894eb4ed9221ebf6218533989
fredrick01 <kigongobazirafred@gmail.com>
2025-03-17 11:40:55 +0300

    changed the urls.py

--- AITS/backend/issues/urls.py
+++ AITS/backend/issues/urls.py
@@ -1,9 +1,7 @@
-# issues/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet, IssueViewSet, CommentViewSet, NotificationViewSet, AuditLogViewSet
-from .views import IssueAttachmentViewSet, StudentRegistrationView
+from .views import IssueAttachmentViewSet, StudentRegistrationView, StudentProfileView # Corrected import
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Initialize the router
@@ -19,4 +17,5 @@ urlpatterns = [
     path('', include(router.urls)),  # Include the router URLs for the viewsets
     path('register/', StudentRegistrationView.as_view(), name='student-registration'),  # Endpoint for student registration
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #add login url
+    path('student/profile/', StudentProfileView.as_view(), name='student-profile'), #Corrected
 ]
