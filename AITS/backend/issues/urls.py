99bb233b51b85efa77eb552f6756ea20c0b1de0c
SSEBALEKE JOHN <jsvico100@gmail.com>
2025-03-19 20:34:06 +0300

    Resolved merge conflicts

--- AITS/backend/issues/urls.py
+++ AITS/backend/issues/urls.py
@@ -17,5 +17,9 @@ urlpatterns = [
     path('', include(router.urls)),  # Include the router URLs for the viewsets
     path('register/', StudentRegistrationView.as_view(), name='student-registration'),  # Endpoint for student registration
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), #add login url
+<<<<<<< HEAD
     path('student/profile/', StudentProfileView.as_view(), name='student-profile'), #Corrected
+=======
+    path('auth/', include('authentication.urls')), 
+>>>>>>> 1fb46bfa (authentication)
 ]
