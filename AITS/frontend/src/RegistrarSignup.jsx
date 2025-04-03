007cc0db0968efd36bbaac3be87e14b2870a83c8
fredrick01 <kigongobazirafred@gmail.com>
2025-04-03 21:18:10 +0300

    hello

--- AITS/frontend/src/RegistrarSignup.jsx
+++ AITS/frontend/src/RegistrarSignup.jsx
@@ -195,6 +195,7 @@ const RegistrarSignup = () => {
                             <option value="College of Engineering">College of Engineering</option>
                             <option value="College of Medicine">College of Medicine</option>
                             <option value="College of Business">College of Business</option>
+                            <option value="School of Law">College of Business</option>
                         </Select>
                     </FormControl>
                     
