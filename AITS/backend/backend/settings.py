722dcf2634c8a3d2320a169529575e0a2716937d
Mutsaka Emmason <mutsakaemmason@gmail.com>
2025-04-04 13:48:33 +0300

    Addedfrontend build,updated settings for staticfiles

--- AITS/backend/backend/settings.py
+++ AITS/backend/backend/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path,os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-49a@$=$ontb06%au1wsyjm=1i4#(gbgfmm1$d+6#_i_l(^-^vp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['MutsakaEmmason.pythonanywhere.com']
 
 
 # Application definition
@@ -152,6 +155,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'dist'),  
+]
 
 # Default primary key field type.
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
