a8855ad564c348f0ad18151782ed594e0734123f
fredrick01 <kigongobazirafred@gmail.com>
2025-04-07 13:32:32 +0300

    added the issues to be displayed to the registrars

--- AITS/env/Scripts/activate.bat
+++ AITS/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\HP\Desktop\conflicts\Academic-Issue-Tracking-System\AITS\env"
+set "VIRTUAL_ENV=C:\Users\HP PC\Desktop\2\Academic-Issue-Tracking-System\AITS\env"
 
 if not defined PROMPT set PROMPT=$P$G
 
