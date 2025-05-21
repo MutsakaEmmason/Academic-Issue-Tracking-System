297400022fb8b721ad059a8c6a02257f5e6e4e5b
MutsakaEmmason <mutsakaemmason@gmail.com>
2025-05-21 10:02:19 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -164,5 +164,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'emmasonmutsaka@gmail.com'
-EMAIL_HOST_PASSWORD = 'emmason2023' # Use App Password in production
+EMAIL_HOST_PASSWORD = 'cewk tkgj bdcj rqgi' # Use App Password in production
 EMAIL_USE_SSL = False
