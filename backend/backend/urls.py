26d5dd4820095aaa0e5b7629fd32751dada4b177
KIGONGO BAZIRA FRED <kigongobazirafred@gmail.com>
2025-05-23 19:28:23 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from django.conf import settings
 from django.conf.urls.static import static
-from issues.views import GetCSRFToken
+from issues.views import GetCSRFToken,CustomTokenObtainPairView
 
 def home(request):
     return HttpResponse("Welcome to the Academic Issue Tracking System!")
@@ -13,10 +13,11 @@ def home(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('issues.urls')),  # Including your app's URLs here
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Token Obtain
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # Token Obtain
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token Refresh
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),  # Token Verify
     path('api/csrf-token/', GetCSRFToken.as_view(), name='csrf-token'),
+    
     path('', TemplateView.as_view(template_name="index.html")),
     
 ]
