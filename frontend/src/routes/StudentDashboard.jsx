321c96688e82582c2a3988d3c8e252f865a9c063
KIGONGO BAZIRA FRED <kigongobazirafred@gmail.com>
2025-05-26 20:19:20 +0300

    Update StudentDashboard.jsx

--- frontend/src/routes/StudentDashboard.jsx
+++ frontend/src/routes/StudentDashboard.jsx
@@ -18,7 +18,7 @@ import {
 } from '@chakra-ui/react';
 import Footer from '../components/Footer';
 import AboutUs from '../components/AboutUs';
-const BASE_URL = 'https://academic-issue-tracking-system-ba1p.onrender.com';
+const BASE_URL = 'https://academic-issue-tracking-system-gbch.onrender.com';
 
 const StudentDashboard = () => {
     const navigate = useNavigate();
