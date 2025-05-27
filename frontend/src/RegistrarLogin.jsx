6c9eddce85d41e86be0f8919c78bf9270aadbbf3
KIGONGO BAZIRA FRED <kigongobazirafred@gmail.com>
2025-05-27 22:37:29 +0300

    Update RegistrarLogin.jsx

--- frontend/src/RegistrarLogin.jsx
+++ frontend/src/RegistrarLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Box, Button, FormControl, FormLabel, Input, VStack, Text, useToast } from "@chakra-ui/react";
